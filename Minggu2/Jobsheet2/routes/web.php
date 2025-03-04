c3044207e3eb84f6c5e68907432adc40fbdd95ff
AlKhrisW <aldokhrisna@gmail.com>
2025-03-04 11:20:00 +0700

    View

--- Minggu2/Jobsheet2/routes/web.php
+++ Minggu2/Jobsheet2/routes/web.php
@@ -61,17 +61,28 @@ use App\Http\Controllers\PhotoController;
 // Route::get('/about', [PageController::class,'about']);
 // Route::get('/articles/{id}', [PageController::class,'articles']);
 
-Route::get('/', [HomeController::class,'index']);
+// Route::get('/', [HomeController::class,'index']);
 
-Route::get('/about', [AboutController::class,'about']);
+// Route::get('/about', [AboutController::class,'about']);
 
-Route::get('/articles/{id}', [ArticleController::class,'articles']);
+// Route::get('/articles/{id}', [ArticleController::class,'articles']);
 
 // Resource Controller
-Route::resource('photos', PhotoController::class);
-Route::resource('photos', PhotoController::class)->only([
-    'index', 'show'
-]);
-Route::resource('photos', PhotoController::class)->except([
-    'create', 'store', 'update', 'destroy'
-]);   
+// Route::resource('photos', PhotoController::class);
+// Route::resource('photos', PhotoController::class)->only([
+//     'index', 'show'
+// ]);
+// Route::resource('photos', PhotoController::class)->except([
+//     'create', 'store', 'update', 'destroy'
+// ]);
+
+// view
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'AldoK']);
+// });
+
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'AldoK(.blog)']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
