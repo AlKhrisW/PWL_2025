9f0030721c728bd445a1ea48999f690867473404
AlKhrisW <aldokhrisna@gmail.com>
2025-03-04 10:45:02 +0700

    Controller

---
+++ Minggu2/Jobsheet2/app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // public function index() {
+    //     return 'Selamat Datang';
+    // }
+    // public function about() {
+    //     return 'Nama: Aldo Khrisna Wijaya, NIM: 2341760091';
+    // }
+    // public function articles($id) {
+    //     return 'Halaman Artikel dengan Id '.$id;
+    // }
+}
--- Minggu2/Jobsheet2/routes/web.php
+++ Minggu2/Jobsheet2/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,36 +19,59 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 //  Basic Route
-Route::get('/hello', function() {
-    return 'Hello World';
-});
-Route::get('/world', function() {
-    return 'World';
-});
-Route::get('/', function() {
-    return 'Selamat Datang';
-});
-Route::get('/about', function() {
-    return 'NIM: 2341760091, Nama: Aldo Khrisna Wijaya';
-});
+// Route::get('/hello', function() {
+//     return 'Hello World';
+// });
+// Route::get('/world', function() {
+//     return 'World';
+// });
+// Route::get('/', function() {
+//     return 'Selamat Datang';
+// });
+// Route::get('/about', function() {
+//     return 'NIM: 2341760091, Nama: Aldo Khrisna Wijaya';
+// });
 
 // Route dengan Parameters
 // Route::get('/user/{name}', function ($name) {
 //     return 'Nama saya '.$name;
 // });
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID '.$id;
-});
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+// });
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID '.$id;
+// });
 
 // Optional Parameters
-Route::get('user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-});
+// Route::get('user/{name?}', function ($name='John') {
+//     return 'Nama saya '.$name;
+// });
+
+// WelcomeController
+// Route::get('/hello', [WelcomeController::class,'hello']);
+
+// PageController
+// Route::get('/', [PageController::class,'index']);
+// Route::get('/about', [PageController::class,'about']);
+// Route::get('/articles/{id}', [PageController::class,'articles']);
+
+Route::get('/', [HomeController::class,'index']);
+
+Route::get('/about', [AboutController::class,'about']);
+
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
+
+// Resource Controller
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);   
