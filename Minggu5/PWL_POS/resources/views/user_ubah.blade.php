bd4a2e36bf87bc60ef5c640c01634f544326964f
AlKhrisW <aldokhrisna@gmail.com>
2025-03-15 10:14:48 +0700

    Praktikum 1

---
+++ Minggu5/PWL_POS/resources/views/user_ubah.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Form Tambah Data User</h1>
+        <form action="/user/ubah_simpan/{{$data->user_id}}" method="POST">
+            {{csrf_field()}}
+            {{method_field('PUT')}}
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Masukan Username" value="{{$data->username}}">
+            <br>
+            <label>Nama</label>
+            <input type="text" name="nama" placeholder="Masukan Nama" value="{{$data->nama}}">
+            <br>
+            <label>Password</label>
+            <input type="text" name="password" placeholder="Masukan Password" value="{{$data->password}}">
+            <br>
+            <label>Level ID</label>
+            <input type="text" name="level_id" placeholder="Masukan ID Level" value="{{$data->level_id}}">
+            <br><br>
+            <input type="submit" value="Simpan" class="btn btn-success">
+        </form>
+    </body>
+</html>
