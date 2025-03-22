b4f1d94c8f71a2de196747ae119d85cabee4a7ad
AlKhrisW <aldokhrisna@gmail.com>
2025-03-22 21:33:29 +0700

    Tugas Tabel m_supplier

--- Minggu5.1/PWL_POS/database/seeders/StokSeeder.php
+++ Minggu5.1/PWL_POS/database/seeders/StokSeeder.php
@@ -15,7 +15,8 @@ class StokSeeder extends Seeder
     {
         $data = [
             [
-                'stok_id' => 1, 
+                'stok_id' => 1,
+                'supplier_id' => 1,
                 'barang_id' => 1, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -23,6 +24,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 2, 
+                'supplier_id' => 2,
                 'barang_id' => 2, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -30,6 +32,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 3, 
+                'supplier_id' => 3,
                 'barang_id' => 3, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -37,6 +40,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 4, 
+                'supplier_id' => 4,
                 'barang_id' => 4, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -44,6 +48,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 5, 
+                'supplier_id' => 5,
                 'barang_id' => 5, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -51,6 +56,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 6, 
+                'supplier_id' => 6,
                 'barang_id' => 6, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -58,6 +64,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 7, 
+                'supplier_id' => 7,
                 'barang_id' => 7, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -65,6 +72,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 8, 
+                'supplier_id' => 8,
                 'barang_id' => 8, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -72,6 +80,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 9, 
+                'supplier_id' => 9,
                 'barang_id' => 9, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
@@ -79,6 +88,7 @@ class StokSeeder extends Seeder
             ],
             [
                 'stok_id' => 10, 
+                'supplier_id' => 10,
                 'barang_id' => 10, 
                 'user_id' => 1, 
                 'stok_tanggal' => '2025-03-05 08:00:00', 
