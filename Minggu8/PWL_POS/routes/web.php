125a0a5748c4b418cee9f6e8d7a1c7ba1786606f
AlKhrisW <aldokhrisna@gmail.com>
2025-04-06 10:30:27 +0700

    Tugas 3

--- Minggu8/PWL_POS/routes/web.php
+++ Minggu8/PWL_POS/routes/web.php
@@ -57,6 +57,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/import', [UserController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [UserController::class, 'import_ajax']); // ajax import excel
             Route::get('/export_excel', [UserController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [UserController::class, 'export_pdf']); // export pdf
         });
         
         Route::group(['prefix' => 'level'], function () {
@@ -71,6 +72,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/import', [LevelController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [LevelController::class, 'import_ajax']); // ajax import excel
             Route::get('/export_excel', [LevelController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [LevelController::class, 'export_pdf']); // export pdf
         });
         
         Route::group(['prefix' => 'supplier'], function () {
@@ -83,6 +85,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/import', [SupplierController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [SupplierController::class, 'import_ajax']); // ajax import excel
             Route::get('/export_excel', [SupplierController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [SupplierController::class, 'export_pdf']); // export pdf
         });
         
         Route::group(['prefix' => 'kategori'], function () {
@@ -95,6 +98,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/import', [KategoriController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [KategoriController::class, 'import_ajax']); // ajax import excel
             Route::get('/export_excel', [KategoriController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [KategoriController::class, 'export_pdf']); // export pdf
         });
     });
     
