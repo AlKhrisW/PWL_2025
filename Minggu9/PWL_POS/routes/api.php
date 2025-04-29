bb568be06d5db644c060dc947d914ae3d66bc064
AlKhrisW <aldokhrisna@gmail.com>
2025-04-29 19:33:40 +0700

    Praktikum 2

--- Minggu9/PWL_POS/routes/api.php
+++ Minggu9/PWL_POS/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', \App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
